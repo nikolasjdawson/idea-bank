386fcf7f594064fafaf414395c36f8ce6f0da98d
Pete Argento <news@peterargent.com>
2018-12-13 09:08:33 +0000

    third_commit

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,6 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   
   validates :title, :problem, :people, :solution, :presence => true
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :ideas
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
